ea1e5daf099f26deb5c720dca2c2d564a4e16a1b
MartaMunMol <marmunmol@gmail.com>
2022-12-14 14:54:19 +0100

    actualizo rama feature

---
+++ index.js
@@ -0,0 +1,22 @@
+document.querySelector('#boton-anadir').addEventListener('click', agregar);
+
+let nombres = [];
+
+function agregar(){
+    let nombres=document.querySelector('#nombre').value;
+
+    nombres.push(nombre);
+    console.log(nombres);
+
+    mostrar();
+
+    document.querySelector('nombres').value = "";
+}
+
+function mostrar (){
+    let lista =document.querySelector('#texto-instructions');
+    lista.innerHTML = '';
+for (let elementoActual of nombres) {
+    lista.innerHTML += "<li>" + elementoActual + "</li>"
+}
+}
