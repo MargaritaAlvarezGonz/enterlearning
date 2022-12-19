5ee39264fe68fa5d3584a6230c18837b13d0c4c8
MariaGF <miriangimenez@gmail.com>
2022-12-19 09:57:46 +0100

    modifiedSection

---
+++ fotos.js
@@ -0,0 +1,7 @@
+var listaFotos = document.querySelector('#listaFotos');
+for (let i= 0; i=24;i++) {
+  listaFotos.innerHTML = '<p> eeeeee </p>';
+}
+
+
+//<img src="./imagenes/calendarioDeAdviento/Dia ${i}.png" alt="" class=''>
