5dabb030e353c2ad9dd49a6587e1f362eda17053
MariaGF <miriangimenez@gmail.com>
2022-12-19 13:43:07 +0100

    CambiosMarta

--- fotos.js
+++ fotos.js
@@ -1,6 +1,9 @@
-var listaFotos = document.querySelector('#listaFotos');
-for (let i= 1; i<26;i++) {
+let listaFotos = document.querySelector('#listaFotos');
+for (let i = 1; i < 26; i++) {
   listaFotos.innerHTML += `<img src="./imagenes/calendarioDeAdviento/Dia${i}.png" alt="" class='estilo'>`;
 }
 
-
+function seleccionImgAleatoria(listaFotos) {
+  return listaFotos[] 
+}
+console.log(seleccionImgAleatoria(listaFotos));
